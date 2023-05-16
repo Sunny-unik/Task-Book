da88a57149bc6b6a44f08a3e0b36486a0d7a6dda
Siddarth Mewara <siddarth.mewara597@gmail.com>
2023-05-16 23:08:19 +0530

    setUp routes

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login part here</h1>
+    </div>
+  );
+}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,18 @@
+import { NavLink } from "react-router-dom";
+// import Signup from "./SignUp";
+import User from "./User";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <nav>
+        <div className="sd-SignUp">
+          <NavLink className="sd-signup" to="/signup">
+            signup here
+          </NavLink>
+        </div>
+        <User />
+      </nav>
+    </div>
+  );
 }
