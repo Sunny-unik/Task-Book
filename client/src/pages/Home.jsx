a98771b1802578bcc8f3b72a5bd2c0352103da42
Siddarth Mewara <siddarth.mewara597@gmail.com>
2023-05-17 10:53:31 +0530

    List component added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,25 @@
 export default function Home() {
-  return <div>Home</div>;
+  let todoList = [
+    "complete Task",
+    "upload code to github",
+    "host project",
+    "send it to the company"
+  ];
+  return (
+    <div className="">
+      <div className="task-list-container border" style={{ width: "20vw", height: "10vw" }}>
+        <div className="list-name">
+          <p>{"List name"}</p>
+        </div>
+        {todoList.map((task, index) => {
+          return (
+            <div className="task-name" key={index}>
+              <input type="checkbox" className="task-status-checkbox" id={"task-status" - index} />
+              <label>{task}</label>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
